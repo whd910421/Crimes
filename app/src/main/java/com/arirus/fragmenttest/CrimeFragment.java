b9d168f8cdeaf00cdc86440af1f5bec78b40af21
Arirus <799937040@qq.com>
2016-09-23 14:42:21 +0800

    Crimes-0014 第二个提升 不要运行,有问题  Part

--- app/src/main/java/com/arirus/fragmenttest/CrimeFragment.java
+++ app/src/main/java/com/arirus/fragmenttest/CrimeFragment.java
@@ -16,6 +16,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.ShareCompat;
+import android.support.v4.content.ContextCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -158,16 +159,21 @@ public class CrimeFragment extends Fragment implements View.OnClickListener {
                 startActivity(i);
                 break;
             case R.id.button_open_app :
-                startActivityForResult(pickContact,REQUEST_CONTACT);
+                int permissionCheck = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS);
+                if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+                } else {
+                    startActivityForResult(pickContact, REQUEST_CONTACT);
+                }
                 break;
             case R.id.button_call:
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
-                    //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
-                } else {
+//                if ( mSuspectID == null ) return;
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+//                    requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+//                    //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+//                } else {
                     ShowTel();
-                }
+//                }
 
 
             default:
@@ -176,13 +182,11 @@ public class CrimeFragment extends Fragment implements View.OnClickListener {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission is granted
-                ShowTel();
-            } else {
-                Toast.makeText(getActivity(), "Until you grant the permission, we canot display the names", Toast.LENGTH_SHORT).show();
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent pickContact = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(pickContact, REQUEST_CONTACT);
             }
         }
     }
@@ -193,7 +197,7 @@ public class CrimeFragment extends Fragment implements View.OnClickListener {
         ContentResolver contentResolver = getActivity().getContentResolver();
         Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
                 null, null, null, null);
-        while(cursor.moveToNext())
+        while(cursor.moveToFirst())
         {
             Cursor phoneCursor = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                     null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID+"="+mSuspectID, null, null);
